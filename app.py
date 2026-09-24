23a0b6486e55b3394d8f4974c7aa06c055bed603
Nguyễn Trần Vân Anh <layla@Layla-3.local>
2026-09-24 10:34:44 +0700

    doi mau pastel

--- app.py
+++ app.py
@@ -120,7 +120,7 @@ if 'ngay' in df_hien_thi.columns:
             theta=alt.Theta('so_luong:Q'),
             color=alt.Color('trang_thai:N', 
                             scale=alt.Scale(domain=['Còn hàng', 'Hết hàng', 'Âm kho'], 
-                                            range=["#a8e6cf", "#ffdfba", "#ffb3ba"]),
+                                            range=["#95afdd", "#ffdfba", "#ffb3ba"]),
                             legend=alt.Legend(title=None, orient="bottom")),
             tooltip=['trang_thai', 'so_luong']
         )
